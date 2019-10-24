6cc057abb3eb078e7452aab0387fdc916f75212f
JingMingChan <prochan75103161@gmail.com>
2019-10-24 20:32:13 +0800

    diceroler ver 5

--- app/src/main/java/com/example/pratical1wed2/MainActivity.kt
+++ app/src/main/java/com/example/pratical1wed2/MainActivity.kt
@@ -17,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
         val countButton: Button = findViewById(R.id.count_button)
-        countButton.setOnClickListener { count()}
+        countButton.setOnClickListener { countUp()}
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener { reset()}
     }
 
     private fun rollDice(){
@@ -27,32 +29,54 @@ class MainActivity : AppCompatActivity() {
         val resultText1: TextView = findViewById(R.id.result_text1)
         val resultText2: TextView = findViewById(R.id.result_text2)
         val resultText3: TextView = findViewById(R.id.result_text3)
-        val resultText4: TextView = findViewById(R.id.result_text4)
+
         val randomInt1 = Random().nextInt(6) + 1
         val randomInt2 = Random().nextInt(6) + 1
         val randomInt3 = Random().nextInt(6) + 1
         resultText1.text = randomInt1.toString()
         resultText2.text = randomInt2.toString()
         resultText3.text = randomInt3.toString()
-        resultText4.text = ""
+
     }
 
 
-    private fun count(){
+    private fun countUp(){
 
         val resultText1: TextView = findViewById(R.id.result_text1)
         val resultText2: TextView = findViewById(R.id.result_text2)
         val resultText3: TextView = findViewById(R.id.result_text3)
-        val resultText4: TextView = findViewById(R.id.result_text4)
 
-        try {
-            resultText4.text = (resultText1.text.toString().toInt() + resultText2.text.toString().toInt() + resultText3.text.toString().toInt()).toString()
-            Toast.makeText(this, "Dices Total Value Counted",
-                Toast.LENGTH_SHORT).show()
-        }catch(e: Exception){
-            Toast.makeText(this, "Roll Dice First",
-                Toast.LENGTH_SHORT).show()
+
+        if(resultText1.text =="Hello World!"){
+            resultText1.text = "1"
+        }else{
+            if(resultText1.text.toString().toInt() < 6){
+                resultText1.text = (resultText1.text.toString().toInt() +1).toString()
+            }
+        }
+        if(resultText2.text =="Hello World!"){
+            resultText2.text = "1"
+        }else{
+            if(resultText2.text.toString().toInt() < 6){
+                resultText2.text = (resultText2.text.toString().toInt() +1).toString()
+            }
         }
 
+        if(resultText3.text =="Hello World!"){
+            resultText3.text = "1"
+        }else{
+            if(resultText3.text.toString().toInt() < 6){
+                resultText3.text = (resultText3.text.toString().toInt() +1).toString()
+            }
+        }
+    }
+    private fun reset(){
+        val resultText1: TextView = findViewById(R.id.result_text1)
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView = findViewById(R.id.result_text3)
+
+        resultText1.text="Hello World!"
+        resultText2.text="Hello World!"
+        resultText3.text="Hello World!"
     }
 }
